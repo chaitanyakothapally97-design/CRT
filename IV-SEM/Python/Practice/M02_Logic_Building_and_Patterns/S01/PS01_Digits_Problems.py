0db59fe3fce3c964730326862d26821b9c9909e7
Chaitanya <chaitanyakothapally97@gmail.com>
2026-01-28 16:16:32 +0530

    commit and push to CRT

---
+++ IV-SEM/Python/Practice/M02_Logic_Building_and_Patterns/S01/PS01_Digits_Problems.py
@@ -0,0 +1,17 @@
+'''n = int(input())
+temp = n
+count = 0
+while n > 0:
+    n //= 10
+    count += 1
+print(count)
+print(len(str(temp)))'''
+# #2 Find the sum of digits of a number
+# n = int(input())
+# s = 0
+# while n > 0:
+#     s += (n % 10)
+#     n //= 10
+# print(s)
+
+n = int(input())
